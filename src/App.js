08610aacff2b3fc637079eab4a31ab2341a48deb
pak <rupaklamadev@gmail.com>
2021-08-12 18:35:55 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const hello = '';
+
   return (
     <div className='App'>
       <header className='App-header'>
---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'value-keyword-case': null,
+    'function-name-case': null,
+    'selector-type-no-unknown': null,
+    'declaration-colon-newline-after': null,
+    indentation: null,
+    'no-eol-whitespace': null,
+    'no-missing-end-of-source-newline': null,
+  },
+};
